0dcd54ce59e5cb1faf20b17480486194e028b3aa
R-AOY <aoyama.dev@outlook.com>
2025-05-07 22:43:18 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleParkingCost(value) {
-  const feeDiv = document.getElementById("parking-fee");
-  feeDiv.style.display = value === "yes" ? "block" : "none";
-}
-
-document.getElementById("user-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("情報が送信されました！（次のステップへ進みましょう）");
-});
